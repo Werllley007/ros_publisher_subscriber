2000221306ccdc8c93fc5d70d0efa72ff922fa9a
Werllley007 <thiagowerlley@ufc.br>
2022-09-09 20:30:06 -0300

    segundo

--- src/ros_publisher_subscriber.cpp
+++ src/ros_publisher_subscriber.cpp
@@ -6,6 +6,9 @@
 #include "nav_msgs/Odometry.h"
 #include "/home/werlley/catkin_ws/devel/include/ros_publisher_subscriber/RoboDistancia.h"
 
+#include "geometry_msgs/Twist.h"
+#include "std_msgs/String.h"
+
 using ros_publisher_subscriber::RoboDistancia;
 using std::string;
 using std::vector;
@@ -20,25 +23,41 @@ public:
 
 class PublishAndSubscribe{
 public:
-    PublishAndSubscribe(const ros::Publisher& landmark_pub): landmarks_(), landmark_pub_(landmark_pub){
+    PublishAndSubscribe(){ //const ros::Publisher& landmark_pub): landmarks_(), landmark_pub_(landmark_pub){
         InicializarLandmark();
+
+        odometry = node_.subscribe("/odom", 10, &PublishAndSubscribe::OdomCallback, this);
+    
+        Robot_pub = node_.advertise<geometry_msgs::Twist>("/mobile_base/commands/velocity", 10);
+        landmark_pub = node_.advertise<RoboDistancia>("/closest_landmark", 10);
+
     }
     void OdomCallback(const nav_msgs::Odometry::ConstPtr& msg){
         double x = msg->pose.pose.position.x;
         double y = msg->pose.pose.position.y;
         RoboDistancia rd = FindClosest(x,y);
                  
-        landmark_pub_.publish(rd);
+        landmark_pub.publish(rd);
+
+        //publica valores de velocidade para o robo navegar sozinho
+        geometry_msgs::Twist velocity;
+        velocity.linear.x = 0.2;
+        velocity.angular.z = 0.2;
+        Robot_pub.publish(velocity);
+
         if(rd.distancia <= 0.5){
-            ROS_INFO("Eu estou proximo a %s", rd.nome.c_str());
+            ROS_INFO("Eu estou proximo: %s", rd.nome.c_str());
         }
     }
 
 
 private:
-    vector<Landmark> landmarks_;
-    ros::Publisher landmark_pub_;
-
+    vector<Landmark> landmarks_;    
+    ros::Publisher Robot_pub;
+    ros::Subscriber odometry;
+    ros::Publisher landmark_pub;
+    ros::NodeHandle node_;
+    
     RoboDistancia FindClosest(double x, double y){
         RoboDistancia resultado;
         resultado.distancia = -1;
@@ -46,8 +65,8 @@ private:
         for(size_t i = 0; i < landmarks_.size(); ++i){
             Landmark landmark = landmarks_[i];
 
-            double xd = landmark.x = x;
-            double yd = landmark.y = y;
+            double xd = landmark.x - x;
+			double yd = landmark.y - y;
             double distance = sqrt(xd*xd + yd*yd);
 
             if(resultado.distancia < 0 || distance < resultado.distancia){
@@ -67,16 +86,12 @@ private:
     }
 };
 
-
-
-
-
 int main(int argc, char** argv){
-    ros::init(argc, argv, "location_monitor");
-    ros::NodeHandle nh;
-    ros::Publisher landmark_pub = nh.advertise<RoboDistancia>("closest_landmark",10);
-    PublishAndSubscribe monitor(landmark_pub);
-    ros::Subscriber sub = nh.subscribe("odom", 10, &PublishAndSubscribe::OdomCallback, &monitor);
+    ros::init(argc, argv, "ros_publisher_subscriber");
+        
+    //PublishAndSubscribe monitor(landmark_pub);
+    PublishAndSubscribe RoboObj;
+    
     ros::spin();
 
     return 0;
